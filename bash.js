106454a3fa679f6cf39479745bb4e72fc6b11726
Depak Borhara <depakborhara@gmail.com>
2019-11-11 14:39:54 -0500

    Cat not working

--- bash.js
+++ bash.js
@@ -1,15 +1,20 @@
-const pwd = require('./pwd')
-const ls = require('./ls')
-process.stdout.write('prompt > ')
-process.stdin.on('data', (data) => {
+const pwd = require('./pwd');
+const ls = require('./ls');
+const cat = require('./cat');
+process.stdout.write('prompt > ');
+process.stdin.on('data', data => {
   let cmd = data.toString().trim();
   if (cmd === 'pwd') {
-    pwd()
-  }
-  else if (cmd === 'ls') {
-    ls()
+    pwd();
+  } else if (cmd === 'ls') {
+    ls();
+  } else if (cmd === 'cat') {
+    process.stdin.on('fileName', fileName => {
+      cat(fileName);
+    })
+    ;
   } else {
     process.stdout.write('you typed:' + cmd);
-    process.stdout.write('\nprompt > ')
+    process.stdout.write('\nprompt > ');
   }
-})
+});
---
+++ cat.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+
+// const cat = data => {
+//   return fs.readFile('./',(err,data))=>{
+//     if(err) {
+//       throw err;
+//     } else {
+//     return data;
+//   }}
+// };
+
+const cat = 'I am working'
+
+module.exports = cat;
